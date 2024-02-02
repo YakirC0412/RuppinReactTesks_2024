67dde2e20bb99f0ff29bc71df58bec0b74b96e79
YakirC0412 <155883698+YakirC0412@users.noreply.github.com>
2024-02-02 19:13:17 +0200

    Tesk1

---
+++ FirstReactTesk/Q1/Q1.js
@@ -0,0 +1,53 @@
+﻿class Counter {
+    //cunstructor
+    constructor() {
+        this.num = 0;
+    }
+
+    //class methods
+    Initialize(InputNum) {
+        this.num = InputNum;
+    }
+
+    Increment() {
+        this.num++;
+    }
+    Go() {
+        let arr = [];
+        for (let index = 0; index <= this.num; index++) {
+            arr.push(index);
+        }
+        ArrayPrint.innerHTML = arr.join(", ");
+    }
+}
+let Input = document.getElementById("InputNum");
+let btnStart = document.getElementById("btnStart");
+let btnPlus = document.getElementById("btnPlus");
+let btnGo = document.getElementById("btnGo");
+let P_array = document.getElementById("ArrayPrint");
+
+//addEventListener = take the element by id and start the function base on the mouse event
+//for exmple: Input= the element from the html page, click = the mouse_event,
+//function= what i want to do after i click
+Input.addEventListener("click", function () {
+    btnPlus.disabled = true;
+    btnGo.disabled = true;
+    ArrayPrint.innerHTML = "";
+});//every time we change numbers on the textBox the print array will disapear
+
+btnStart.addEventListener("click", function () {
+    NewCounter = new Counter();//create a new object
+    NewCounter.Initialize(Input.value);
+    btnPlus.disabled = false;
+    btnGo.disabled = false;
+    //abled the go and + button
+});
+
+btnPlus.addEventListener("click", function () {
+    NewCounter.Increment();
+    Input.value = NewCounter.num;
+});
+
+btnGo.addEventListener("click", function () {
+    NewCounter.Go();
+});
---
+++ FirstReactTesk/Q2/Q2.js
@@ -0,0 +1,76 @@
+﻿class Duck {
+    constructor(name, color, age, weight, image) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+        this.weight = weight;
+        this.image = image;
+
+    }
+
+    Show() {
+        let str = "";
+        str += `<h3>duck details:</h3>`;
+        str += `duck name: ${this.name}<br>`;
+        str += `duck color: <span style="color:${this.color}">${this.color}</span><br>`;
+        str += `duck age: ${this.age}<br>`;
+        str += `duck weight: ${this.weight}<br>`;
+        str += `<img src="${this.image}" alt="duck" width="150" height="150"><br>`;
+        document.getElementById('result-block').innerHTML = str;
+    }
+
+    Quack() {
+        let str = "";
+        document.getElementById('result-block').innerHTML = str;
+        let quackPrintNumber = Number((this.age * this.weight) / 2);
+        for (var i = 0; i < quackPrintNumber; i++) {
+            str += `Quack!, `;
+        }
+        document.getElementById('result-block').innerHTML = str;
+        if (this.audioCounter < 3) {
+            this.audioCounter++;
+            document.getElementById("duck_audio").play();
+            setTimeout(() => this.Quack(), 1500);
+        }
+
+    }
+
+
+}
+let CreateBTN = document.getElementById("create-duck");
+let ShowBTN = document.getElementById("show");
+let QuackBTN = document.getElementById("quack");
+
+CreateBTN.addEventListener('click', function () {
+    const color = document.getElementById('color').value;
+    const name = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const weight = document.getElementById('weight').value;
+    const image = document.getElementById('img').value;
+    duck = new Duck(name, color, age, weight, image);
+    ShowBTN.disabled = false;
+    QuackBTN.disabled = false;
+    CreateBTN.disabled = true;
+});
+
+ShowBTN.addEventListener('click', function () {
+    duck.Show();
+});
+
+QuackBTN.addEventListener('click', function () {
+    duck.audioCounter = 0;
+    duck.Quack();
+
+});
+
+ResetBTN.addEventListener('click', function () {
+        ShowBTN.disabled = true;
+        QuackBTN.disabled = true;
+        CreateBTN.disabled = false;
+});
+
+
+
+
+
+
